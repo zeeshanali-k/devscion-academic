47e3b9152bf904e5ea2a3cbed2e1501883109dac
Zeeshan Ali <72798406+zeeshanali-k@users.noreply.github.com>
2026-04-24 11:15:55 +0500

    Update certifications with new entries and corrections
    Corrected issuer name and date formats for existing certifications, and added two new certifications.

--- src/data/certifications.js
+++ src/data/certifications.js
@@ -3,8 +3,8 @@ export const certifications = [
     {
         id: 1,
         name: 'Machine Learning Specialization',
-        issuer: 'Deeplearning.ai, Standford',
-        date: 'Nov 13, 2022',
+        issuer: 'Deeplearning.ai, Stanford',
+        date: 'November 13, 2022',
         credentialId: '5X3U4GC6PRG7',
         link: 'https://coursera.org/verify/specialization/5X3U4GC6PRG7'
     },
@@ -12,8 +12,24 @@ export const certifications = [
         id: 2,
         name: 'Python for Data Science, AI & Development',
         issuer: 'IBM',
-        date: 'Nov 15, 2025',
+        date: 'November 15, 2025',
         credentialId: 'SVWVD8OZYL5J',
         link: 'https://www.coursera.org/verify/SVWVD8OZYL5J'
+    },
+    {
+        id: 3,
+        name: 'Develop Generative AI Applications: Get Started',
+        issuer: 'IBM',
+        date: 'April 13, 2026',
+        credentialId: 'THLTQWDSGG2P',
+        link: 'https://coursera.org/verify/THLTQWDSGG2P'
+    },
+    {
+        id: 3,
+        name: 'Build RAG Applications: Get Started',
+        issuer: 'IBM',
+        date: 'April 24, 2026',
+        credentialId: 'SKO02I98B0WC',
+        link: 'https://coursera.org/verify/SKO02I98B0WC'
     }
 ];
